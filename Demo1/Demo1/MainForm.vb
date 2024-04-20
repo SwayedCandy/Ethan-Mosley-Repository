7c1183d2d05dfbd715b63026d535f5b96b68bd57
Ethan Mosley <134177756+SwayedCandy@users.noreply.github.com>
2024-04-20 17:01:16 -0400

    Add files via upload

---
+++ Demo1/Demo1/MainForm.vb
@@ -0,0 +1,18 @@
+﻿'Name: Ethan Mosley
+'Date: 5/16/2023
+'Program Name: Einstein's Equation
+'Description: Program to show what equation Einstein is famous for.
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close() ' End the application
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        picEquation.Visible = True ' Display the equation
+    End Sub
+
+    Private Sub btnHide_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+        picEquation.Visible = False ' Stop displaying equation
+    End Sub
+End Class
